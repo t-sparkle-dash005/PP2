c46486bcd96bb56ae3f25dcbcb6fb92cdd47df39
ayazhan.k <a_karatayeva@kbtu.kz>
2026-03-17 03:31:40 +0500

    Delete Practice6 directory

--- Practice6/builtin_funcs/enum_zip_examples.py
+++
@@ -1,7 +0,0 @@
-files = ["demo.txt", "cool_cat.jpg", "garfield.jpg"]
-sizes = ["1KB", "9,99KB", "12,6KB"]
-for index, filename in enumerate(files, start=1):
-    print(f"{index}: {filename}")
-
-for name, size in zip(files, sizes):
-    print(f"{name}: {size} bytes")
--- Practice6/dir_management/move_files.py
+++
@@ -1,6 +0,0 @@
-import os
-import shutil
-
-shutil.move("D:\\PP2\\Practice6\\dir_management\\bsubdir\\dirdir", "D:\\PP2\\Practice6\\dir_management")
-
-shutil.copyfile("D:\\PP2\\Practice6\\dir_management\\bsubdir\\file.txt", "D:\\PP2\\Practice6\\dir_management\\file.txt")
